08a736a4468b426a1da0bbef4f1d619177556b63
Jackson Moraes <jacksontadeu@hotmail.com>
2026-02-03 13:05:10 -0300

    :sparkles: feat: Criacao do projeto

---
+++ src/test/java/br/com/jtmjinfo/CadastroCliente/CadastroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jtmjinfo.CadastroCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
